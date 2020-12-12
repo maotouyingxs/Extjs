23a4afcb8f4c221f237b8ab8c95c7d91a9e74395
maotouyingxs <ren915187670>
2020-12-12 21:03:53 +0800

    删除out文件夹

--- out/artifacts/EXT_war_exploded/base/001_helloworld.js
+++
@@ -1,8 +0,0 @@
-Ext.onReady(function () {
-
-    Ext.MessageBox.alert('我是标题！', 'Hello world!',function () {
-        console.info(this);
-        alert('我是回调函数！');
-    },this);
-
-})
--- out/artifacts/EXT_war_exploded/text.js
+++
@@ -1,5 +0,0 @@
-Ext.onReady(function () {
-
-    Ext.MessageBox.alert('提示信息！', 'Hello world!');
-
-})
