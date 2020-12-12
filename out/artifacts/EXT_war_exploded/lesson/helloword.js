ebcc7e87069ecea4a0179e97de4340ea86681fb1
maotouyingxs <ren915187670>
2020-12-12 20:49:33 +0800

    控制提交代码

--- out/artifacts/EXT_war_exploded/lesson/helloword.js
+++ out/artifacts/EXT_war_exploded/lesson/helloword.js
@@ -1,12 +1,34 @@
 Ext.onReady(function () {
 
-    var src = {name:'张三', age: 20};
-    var config = {name: '李四', sex: '男'};
+    // var src = {name:'张三', age: 20};
+    // var config = {name: '李四', sex: '男'};
+    //
+    //
+    // Ext.apply(src, config);
+    // for (var attr in  src) {
+    //     alert(attr + ': ' + src[attr]);
+    // }
 
 
-    Ext.apply(src, config);
-    for (var attr in  src) {
-        alert(attr + ': ' + src[attr]);
-    }
+    var myArray = [1, 2, 3, 4,-3];
+    Ext.Array.every(myArray, function (item) {
+        if (item > 0) {
+            return true;
+        } else {
+            // alert(item);
+            return false;
+        }
+    },this);
+
+    var newMyArray = Ext.Array.filter(myArray, function (item) {
+        if (item > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },this);
+
+    // alert(newMyArray.join("\n"));
+
 
 });
